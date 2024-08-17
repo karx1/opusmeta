9ebdb1a114c7bc7769e1d753a0513652fee3e40a
Yash Karandikar <nerdstep710@gmail.com>
2024-08-16 22:32:47 -0500

    Use command line argument instead of hardcoding filename

--- examples/read_tags.rs
+++ examples/read_tags.rs
@@ -1,8 +1,8 @@
-use opusmeta::{read_from, Result};
-use std::fs::File;
+use opusmeta::{read_from_path, Result};
 
 fn main() -> Result<()> {
-    let file = File::open("dysti.opus").unwrap();
-    read_from(file)?;
+    let path = std::env::args_os().nth(1).expect("No input file specified");
+    let comments = read_from_path(path)?;
+    println!("{comments:#?}");
     Ok(())
 }
