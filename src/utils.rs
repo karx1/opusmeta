df3b6958c2e523fb0cd960c2f8392e60ddf521ee
Yash Karandikar <nerdstep710@gmail.com>
2025-12-29 16:02:19 -0600

    Fix clippy warnings + Disable clippy nursery

--- src/utils.rs
+++ src/utils.rs
@@ -27,27 +27,31 @@ impl Display for LowercaseString<'_> {
 impl<'a> LowercaseString<'a> {
     /// Create a new `LowercaseString`. This will only allocate if the passed
     /// string isn't lowercase.
-    pub fn from_str(str: &'a str) -> Self {
-        if !str.chars().any(|c| c.is_ascii_uppercase()) {
-            Self(Cow::Borrowed(str))
-        } else {
+    #[must_use]
+    pub fn new_from_str(str: &'a str) -> Self {
+        if str.chars().any(|c| c.is_ascii_uppercase()) {
             Self(Cow::Owned(str.to_ascii_lowercase()))
+        } else {
+            Self(Cow::Borrowed(str))
         }
     }
 
     /// Create a new `LowercaseString`. If the string isn't already lowercase,
     /// this will modify the existing buffer without allocating.
-    pub fn from_string(mut str: String) -> Self {
+    #[must_use]
+    pub fn new_from_string(mut str: String) -> Self {
         str.make_ascii_lowercase();
         Self(Cow::Owned(str))
     }
 
     /// Try to create a new `LowercaseString`. This returns `None` if the passed
     /// string isn't lowercase.
+    #[must_use]
     pub const fn try_from_str(str: &'a str) -> Option<Self> {
         // for loops and iterator/trait methods aren't const stable yet.
         let mut i = 0;
-        while i < str.as_bytes().len() {
+        // len() works here because it returns length in bytes
+        while i < str.len() {
             if str.as_bytes()[i].is_ascii_uppercase() {
                 return None;
             }
@@ -60,7 +64,7 @@ impl<'a> LowercaseString<'a> {
 
 impl<S: AsRef<str>> From<S> for LowercaseString<'static> {
     fn from(str: S) -> Self {
-        Self::from_string(str.as_ref().to_string())
+        Self::new_from_string(str.as_ref().to_string())
     }
 }
 
@@ -120,7 +124,7 @@ mod tests {
 
     #[test]
     fn dont_allocate_already_lowercase_str() {
-        let lower = LowercaseString::from_str("adsf-adsf");
-        assert!(matches!(lower.0, Cow::Borrowed(_)))
+        let lower = LowercaseString::new_from_str("adsf-adsf");
+        assert!(matches!(lower.0, Cow::Borrowed(_)));
     }
 }
